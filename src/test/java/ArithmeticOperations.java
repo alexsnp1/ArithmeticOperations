4d1cbcfa2c7a10c5ef016e565e524192d57ed53d
Aleksandr Maliutin <alexander.hroft@gmail.com>
2025-05-14 00:33:58 +0700

    updated ArithmeticOperations.java

--- src/test/java/ArithmeticOperations.java
+++ src/test/java/ArithmeticOperations.java
@@ -22,10 +22,10 @@ public class ArithmeticOperations {
         System.out.println(a<=b);
         System.out.println(++a);
 
-        System.out.println(Float.MAX_VALUE); // 3.4028235E38
-        System.out.println(Float.MIN_VALUE); // 1.4E-45
-        System.out.println(Double.MAX_VALUE); // 1.7976931348623157E308
-        System.out.println(Double.MIN_VALUE); // 4.9E-324
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
 
         System.out.println(Float.MAX_VALUE*a);
         System.out.println(Double.MIN_VALUE/c);
